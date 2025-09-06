ec80a4846b260f1c983e5ba3b24ce22284158caf
Arunkumar Dhananjayan <arunkd13@gmail.com>
2025-09-06 09:09:18 +0530

    Added scratch queries

---
+++ scratch/queries.sql
@@ -0,0 +1,7 @@
+CREATE VIEW state_code (code, name) AS
+    SELECT substr(id,3), replace(name, 'Governor of ', '')
+    FROM office
+    WHERE id LIKE 'go%'
+/* state_code(code,name) */;
+insert into office (id, name) select concat('com', code), concat(name, ' Council of Ministers') from state_code where code != 'tn';
+insert into supervisor (office_id, relation, supervisor_office_id) select concat('go', code), 'adviser', concat('com', code) from state_code where code != 'tn';
